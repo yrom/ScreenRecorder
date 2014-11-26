5244f860750ddfb486c305fb62e2183374c0b895
yrom <yrom@qq.com>
2014-11-26 22:36:04 +0800

    screen recorder

---
+++ app/src/main/java/net/yrom/screenrecorder/MainActivity.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2014 Yrom Wang <http://www.yrom.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.yrom.screenrecorder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private static final int REQUEST_CODE = 1;
+    private MediaProjectionManager mMediaProjectionManager;
+    private ScreenRecorder mRecorder;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+        //noinspection ResourceType
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+        getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("@@", "onActivityResult:" + requestCode + ", " + resultCode);
+        MediaProjection mediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+        if (mediaProjection == null) {
+            Log.e("@@", "media projection is null");
+            return;
+        }
+        // video size
+        final int width = 1280;
+        final int height = 720;
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "record-" + width + "x" + height + "-" + System.currentTimeMillis() + ".mp4");
+        final int bitrate = 6000000;
+        mRecorder = new ScreenRecorder(width, height, bitrate, 1, mediaProjection, file.getAbsolutePath());
+        mRecorder.start();
+        mButton.setText("Stop Recorder");
+        Toast.makeText(this, "Screen recorder is running...", Toast.LENGTH_SHORT).show();
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mRecorder != null) {
+            mRecorder.quit();
+            mButton.setText("Restart recorder");
+        } else {
+            Intent captureIntent = mMediaProjectionManager.createScreenCaptureIntent();
+            startActivityForResult(captureIntent, REQUEST_CODE);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/net/yrom/screenrecorder/ScreenRecorder.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2014 Yrom Wang <http://www.yrom.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.yrom.screenrecorder;
+
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.media.projection.MediaProjection;
+import android.util.Log;
+import android.view.Surface;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author Yrom
+ */
+public class ScreenRecorder extends Thread {
+    private static final String TAG = "ScreenRecorder";
+
+    private int mWidth;
+    private int mHeight;
+    private int mBitRate;
+    private int mDpi;
+    private String mDstPath;
+    private MediaProjection mMediaProjection;
+    // parameters for the encoder
+    private static final String MIME_TYPE = "video/avc"; // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 30; // 30 fps
+    private static final int IFRAME_INTERVAL = 10; // 10 seconds between I-frames
+    private static final int TIMEOUT_US = 10000;
+
+    private MediaCodec mEncoder;
+    private Surface mSurface;
+    private MediaMuxer mMuxer;
+    private boolean mMuxerStarted = false;
+    private int mVideoTrackIndex = -1;
+    private AtomicBoolean mQuit = new AtomicBoolean(false);
+    private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
+    private VirtualDisplay mVirtualDisplay;
+
+    public ScreenRecorder(int width, int height, int bitrate, int dpi, MediaProjection mp, String dstPath) {
+        super(TAG);
+        mWidth = width;
+        mHeight = height;
+        mBitRate = bitrate;
+        mDpi = dpi;
+        mMediaProjection = mp;
+        mDstPath = dstPath;
+    }
+
+
+    public ScreenRecorder(MediaProjection mp) {
+        // 480p 2Mbps
+        this(640, 480, 2000000, 1, mp, "/sdcard/test.mp4");
+    }
+
+    /**
+     * stop task
+     */
+    public final void quit() {
+        mQuit.set(true);
+    }
+
+    @Override
+    public void run() {
+        try {
+            try {
+                prepareEncoder();
+                mMuxer = new MediaMuxer(mDstPath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            mVirtualDisplay = mMediaProjection.createVirtualDisplay(TAG + "-display",
+                    mWidth, mHeight, mDpi, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+                    mSurface, null, null);
+            Log.d(TAG, "created virtual display: " + mVirtualDisplay);
+            recordVirturalDisplay();
+
+        } finally {
+            release();
+        }
+    }
+
+    private void recordVirturalDisplay() {
+        while (!mQuit.get()) {
+            int index = mEncoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_US);
+            Log.i(TAG, "dequeue output buffer index=" + index);
+            if (index == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                resetOutputFormat();
+
+            } else if (index == MediaCodec.INFO_TRY_AGAIN_LATER) {
+                Log.d(TAG, "retrieving buffers time out!");
+                try {
+                    // wait 10ms
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            } else if (index >= 0) {
+
+                if (!mMuxerStarted) {
+                    throw new IllegalStateException("MediaMuxer dose not call addTrack(format) ");
+                }
+                encodeToVideoTrack(index);
+
+                mEncoder.releaseOutputBuffer(index, false);
+            }
+        }
+    }
+
+    private void encodeToVideoTrack(int index) {
+        ByteBuffer encodedData = mEncoder.getOutputBuffer(index);
+
+        if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            // The codec config data was pulled out and fed to the muxer when we got
+            // the INFO_OUTPUT_FORMAT_CHANGED status.
+            // Ignore it.
+            Log.d(TAG, "ignoring BUFFER_FLAG_CODEC_CONFIG");
+            mBufferInfo.size = 0;
+        }
+        if (mBufferInfo.size == 0) {
+            Log.d(TAG, "info.size == 0, drop it.");
+            encodedData = null;
+        } else {
+            Log.d(TAG, "got buffer, info: size=" + mBufferInfo.size
+                    + ", presentationTimeUs=" + mBufferInfo.presentationTimeUs
+                    + ", offset=" + mBufferInfo.offset);
+        }
+        if (encodedData != null) {
+            encodedData.position(mBufferInfo.offset);
+            encodedData.limit(mBufferInfo.offset + mBufferInfo.size);
+            mMuxer.writeSampleData(mVideoTrackIndex, encodedData, mBufferInfo);
+            Log.i(TAG, "sent " + mBufferInfo.size + " bytes to muxer...");
+        }
+    }
+
+    private void resetOutputFormat() {
+        // should happen before receiving buffers, and should only happen once
+        if (mMuxerStarted) {
+            throw new IllegalStateException("output format already changed!");
+        }
+        MediaFormat newFormat = mEncoder.getOutputFormat();
+
+        Log.i(TAG, "output format changed.\n new format: " + newFormat.toString());
+        mVideoTrackIndex = mMuxer.addTrack(newFormat);
+        mMuxer.start();
+        mMuxerStarted = true;
+        Log.i(TAG, "started media muxer, videoIndex=" + mVideoTrackIndex);
+    }
+
+    private void prepareEncoder() throws IOException {
+
+        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
+
+        Log.d(TAG, "created video format: " + format);
+        mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mSurface = mEncoder.createInputSurface();
+        Log.d(TAG, "created input surface: " + mSurface);
+        mEncoder.start();
+    }
+
+    private void release() {
+        if (mEncoder != null) {
+            mEncoder.stop();
+            mEncoder.release();
+            mEncoder = null;
+        }
+        if (mVirtualDisplay != null) {
+            mVirtualDisplay.release();
+        }
+        if (mMediaProjection != null) {
+            mMediaProjection.stop();
+        }
+        if (mMuxer != null) {
+            mMuxer.stop();
+            mMuxer.release();
+            mMuxer = null;
+        }
+    }
+}
